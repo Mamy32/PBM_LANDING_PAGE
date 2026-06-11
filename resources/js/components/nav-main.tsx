8fc43be73fa7cc5632ac0c69074cb950cf3f4993
Mamy32 <mamymanjakamanana32@gmail.com>
2026-06-12 00:12:58 +0700

    fix sidebar

--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -23,13 +23,16 @@ export function NavMain({ items = [] }: { items: NavItem[] }) {
                             isActive={isCurrentUrl(item.href)}
                             tooltip={{ children: item.title }}
                         >
-                    <Link
+<Link
     href={item.href}
     prefetch
     className="flex items-center gap-2 w-full overflow-hidden"
 >
     {item.icon && <item.icon />}
-    <span className="truncate">{item.title}</span>
+
+    <span className="truncate group-data-[collapsible=icon]:hidden">
+        {item.title}
+    </span>
 </Link>
                         </SidebarMenuButton>
                     </SidebarMenuItem>

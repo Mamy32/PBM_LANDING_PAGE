f36b53417e8e7ce0e89db5b9ee9e8886948031d7
Mamy32 <mamymanjakamanana32@gmail.com>
2026-06-12 13:45:51 +0700

    UI

--- resources/js/pages/landing/success-story.tsx
+++ resources/js/pages/landing/success-story.tsx
@@ -70,7 +70,7 @@ export default function SuccessStory() {
             </div>
 
             {/* RIGHT CARD */}
-            <div className="bg-white p-4 shadow-lg">
+            <div className="p-4 shadow-lg">
                 <img
                     src="/images/shaundju/promote.png"
                     alt="Career Timeline"
